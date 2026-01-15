ed5289c483ce14e68d41cfb9ed5b837a1259a95e
Diego Arce <diegoarmu06@gmail.com>
2026-01-15 12:58:47 -0600

    Commit 18: interfaceSegregation fix

--- solid/interfaceSegregation/fixed/DemoFixed.java
+++ solid/interfaceSegregation/fixed/DemoFixed.java
@@ -1,8 +1,10 @@
 public class DemoFixed {
     public static void main(String[] args) {
         WorkerFixed employee = new EmployeeFixed();
+        WorkerFixed robot = new RobotFixed();
         employee.work();
         ((EmployeeFixed) employee).eat();
         ((EmployeeFixed) employee).sleep();
+        robot.work();
     }
 }
